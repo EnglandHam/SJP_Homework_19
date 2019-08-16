6a64fda37b309e100125bffe3370ea384b09c153
EnglandHam <47395675+EnglandHam@users.noreply.github.com>
2019-08-15 19:34:22 -0500

    added js animation

--- js/index.js
+++ js/index.js
@@ -1,7 +1,28 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
+ScrollReveal().reveal('.gridChild');
 
-$('[data-spy="scroll"]').each(function () {
-  var $spy = $(this).scrollspy('refresh')
-})
+function typeEffect(element, speed) {
+	var text = element.innerHTML;
+	element.innerHTML = "";
+	
+	var i = 0;
+	var timer = setInterval(function() {
+    if (i < text.length) {
+      element.append(text.charAt(i));
+      i++;
+    } else {
+      clearInterval(timer);
+    }
+  }, speed);
+}
+
+
+// application
+var speed = 75;
+var h1 = document.querySelector('h1');
+var delay = h1.innerHTML.length * speed + speed;
+
+// type affect to header
+typeEffect(h1, speed);
